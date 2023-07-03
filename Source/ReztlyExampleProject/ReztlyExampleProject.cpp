d490e52e4878d64c06ccafb785e0a6d647eb0a23
Matthew Zane <mzane@ucsd.edu>
2023-07-03 17:46:53 +0900

    Add Reztly Example Project Structure

---
+++ Source/ReztlyExampleProject/ReztlyExampleProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ReztlyExampleProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReztlyExampleProject, "ReztlyExampleProject" );
---
+++ Source/ReztlyExampleProject/ReztlyExampleProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReztlyExampleProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REZTLYEXAMPLEPROJECT_API AReztlyExampleProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
